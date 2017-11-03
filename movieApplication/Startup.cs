dd7594e79758f337c4c4fddadbd7974101e9c793
joderrick1022 <joderrick1022>
2017-11-03 14:34:39 -0400

    movie app with mel

---
+++ movieApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(movieApplication.Startup))]
+namespace movieApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
